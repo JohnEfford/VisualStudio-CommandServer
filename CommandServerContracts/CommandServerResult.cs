5fe950ab929798aded2e808ef42f362bc8353932
John Efford <john.efford@puregym.com>
2022-08-14 18:27:15 +0100

    Sorted out content

---
+++ CommandServerContracts/CommandServerResult.cs
@@ -0,0 +1,21 @@
+﻿namespace CommandServerContracts
+{
+    public class CommandServerResult
+    {
+        public readonly string Message;
+        public readonly string Payload;
+
+        /// <summary>
+        /// Nothing to return to command server
+        /// </summary>
+        public const string None = "";
+
+        public CommandServerResult(
+            string message,
+            string payload)
+        {
+            Message = message;
+            Payload = payload;
+        }
+    }
+}
--- CommandServerContracts/ICommandServerDispatch.cs
+++ CommandServerContracts/ICommandServerDispatch.cs
@@ -4,25 +4,6 @@ using Newtonsoft.Json.Linq;
 
 namespace CommandServerContracts
 {
-    public class CommandServerResult
-    {
-        public readonly string Message;
-        public readonly string Payload;
-
-        /// <summary>
-        /// Nothing to return to command server
-        /// </summary>
-        public const string None = "";
-
-        public CommandServerResult(
-            string message,
-            string payload)
-        {
-            Message = message;
-            Payload = payload;
-        }
-    }
-
     /// <summary>
     /// Command dispatch which plugs into the Command Server plug in
     /// </summary>
